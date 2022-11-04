dc475868125f734b5dbb5ecffac56d59498576d0
Jared Hanson <jaredhanson@gmail.com>
2022-11-04 09:11:48 -0700

    WIP

--- publish.js
+++ publish.js
@@ -2,16 +2,20 @@ var helper = require('jsdoc/util/templateHelper');
 var taffy = require('taffydb').taffy;
 var path = require('jsdoc/path');
 const template = require('jsdoc/template');
-const fs = require('fs');
+var fs = require('jsdoc/fs');
+var util = require('util');
+
+var htmlsafe = helper.htmlsafe;
+
+// https://github.com/jsdoc/jsdoc/tree/main/packages/jsdoc/templates
 
 var data;
 var view;
 
+var outdir = path.normalize(env.opts.destination);
 
-// https://github.com/jsdoc/jsdoc/tree/main/packages/jsdoc/templates
-
-function mkdirpSync(filepath) {
-  return fs.mkdirSync(filepath, { recursive: true });
+function find(spec) {
+  return helper.find(data, spec);
 }
 
 function hashToLink(doclet, hash) {
@@ -25,6 +29,148 @@ function hashToLink(doclet, hash) {
   url = url.replace(/(#.+|$)/, hash);
 
   return '<a href="' + url + '">' + hash + '</a>';
+}
+
+function needsSignature(doclet) {
+  var needsSig = false;
+
+  // function and class definitions always get a signature
+  if (doclet.kind === 'function' || doclet.kind === 'class') {
+    needsSig = true;
+  }
+  // typedefs that contain functions get a signature, too
+  else if (doclet.kind === 'typedef' && doclet.type && doclet.type.names &&
+    doclet.type.names.length) {
+    for (var i = 0, l = doclet.type.names.length; i < l; i++) {
+      if (doclet.type.names[i].toLowerCase() === 'function') {
+        needsSig = true;
+        break;
+      }
+    }
+  }
+  // and namespaces that are functions get a signature (but finding them is a
+  // bit messy)
+  else if (doclet.kind === 'namespace' && doclet.meta && doclet.meta.code &&
+    doclet.meta.code.type && doclet.meta.code.type.match(/[Ff]unction/)) {
+    needsSig = true;
+  }
+
+  return needsSig;
+}
+
+function getSignatureAttributes(item) {
+  var attributes = [];
+
+  if (item.optional) {
+    attributes.push('opt');
+  }
+
+  if (item.nullable === true) {
+    attributes.push('nullable');
+  }
+  else if (item.nullable === false) {
+    attributes.push('non-null');
+  }
+
+  return attributes;
+}
+
+function updateItemName(item) {
+  var attributes = getSignatureAttributes(item);
+  var itemName = item.name || '';
+
+  if (item.variable) {
+    itemName = '&hellip;' + itemName;
+  }
+
+  if (attributes && attributes.length) {
+    itemName = util.format( '%s<span class="signature-attributes">%s</span>', itemName,
+      attributes.join(', ') );
+  }
+
+  return itemName;
+}
+
+function addParamAttributes(params) {
+  return params.filter(function(param) {
+    return param.name && param.name.indexOf('.') === -1;
+  }).map(updateItemName);
+}
+
+function buildAttribsString(attribs) {
+  var attribsString = '';
+
+  if (attribs && attribs.length) {
+    attribsString = htmlsafe( util.format('(%s) ', attribs.join(', ')) );
+  }
+
+  return attribsString;
+}
+
+function addSignatureParams(f) {
+  var params = f.params ? addParamAttributes(f.params) : [];
+
+  f.signature = util.format( '%s(%s)', (f.signature || ''), params.join(', ') );
+}
+
+function addSignatureReturns(f) {
+  var attribs = [];
+  var attribsString = '';
+  var returnTypes = [];
+  var returnTypesString = '';
+  var source = f.yields || f.returns;
+
+  // jam all the return-type attributes into an array. this could create odd results (for example,
+  // if there are both nullable and non-nullable return types), but let's assume that most people
+  // who use multiple @return tags aren't using Closure Compiler type annotations, and vice-versa.
+  if (source) {
+    source.forEach(function(item) {
+      helper.getAttribs(item).forEach(function(attrib) {
+        if (attribs.indexOf(attrib) === -1) {
+          attribs.push(attrib);
+        }
+      });
+    });
+
+    attribsString = buildAttribsString(attribs);
+  }
+
+  if (source) {
+    returnTypes = addNonParamAttributes(source);
+  }
+  if (returnTypes.length) {
+    returnTypesString = util.format( ' &rarr; %s{%s}', attribsString, returnTypes.join('|') );
+  }
+
+  f.signature = '<span class="signature">' + (f.signature || '') + '</span>' +
+      '<span class="type-signature">' + returnTypesString + '</span>';
+}
+
+function addAttribs(f) {
+  var attribs = helper.getAttribs(f);
+  var attribsString = buildAttribsString(attribs);
+
+  f.attribs = util.format('<span class="type-signature">%s</span>', attribsString);
+}
+
+function shortenPaths(files, commonPrefix) {
+  Object.keys(files).forEach(function(file) {
+    files[file].shortened = files[file].resolved.replace(commonPrefix, '')
+      // always use forward slashes
+      .replace(/\\/g, '/');
+  });
+
+  return files;
+}
+
+function getPathFromDoclet(doclet) {
+  if (!doclet.meta) {
+    return null;
+  }
+
+  return doclet.meta.path && doclet.meta.path !== 'null' ?
+    path.join(doclet.meta.path, doclet.meta.filename) :
+    doclet.meta.filename;
 }
 
 function generate(title, docs, filename, resolveLinks, outdir, dependencies) {
@@ -77,8 +223,13 @@ exports.publish = function(taffyData, opts) {
   //console.log(data());
   
   var conf;
+  var fromDir;
   var globalUrl;
   var indexUrl;
+  var packageInfo;
+  var sourceFilePaths = [];
+  var sourceFiles = {};
+  var staticFiles;
   var templatePath;
   
   data = taffyData;
@@ -100,12 +251,6 @@ exports.publish = function(taffyData, opts) {
       path.basename(conf.default.layoutFile) ) :
       'layout.tmpl';
   
-      console.log('#### VIEW LAYOUT');
-      console.log(view.layout);
-  
-  
-  var outdir = path.normalize(opts.destination);
-  
 
   data = helper.prune(data);
   data.sort('longname, version, since');
@@ -113,16 +258,14 @@ exports.publish = function(taffyData, opts) {
   
   
   data().each(function(doclet) {
-    //console.log(doclet)
+    var sourcePath;
     
     doclet.attribs = '';
   
     if (doclet.examples) {
-      console.log("**** EXAMPLES!")
-      
-      doclet.examples = doclet.examples.map((example) => {
-        let caption;
-        let code;
+      doclet.examples = doclet.examples.map(function(example) {
+        var caption;
+        var code;
 
         if (example.match(/^\s*<caption>([\s\S]+?)<\/caption>(\s*[\n\r])([\s\S]+)$/i)) {
           caption = RegExp.$1;
@@ -131,42 +274,90 @@ exports.publish = function(taffyData, opts) {
 
         return {
           caption: caption || '',
-          code: code || example,
+          code: code || example
         };
       });
     }
     if (doclet.see) {
-      //console.log("SEE")
-      
-      doclet.see.forEach((seeItem, i) => {
-              doclet.see[i] = hashToLink(doclet, seeItem, dependencies);
-        });
+      doclet.see.forEach(function(seeItem, i) {
+        doclet.see[i] = hashToLink(doclet, seeItem);
+      });
     }
+    
+    // build a list of source files
     if (doclet.meta) {
-      //console.log("META")
-      //console.log(doclet)
+      sourcePath = getPathFromDoclet(doclet);
+      sourceFiles[sourcePath] = {
+        resolved: sourcePath,
+        shortened: null
+      };
+      if (sourceFilePaths.indexOf(sourcePath) === -1) {
+        sourceFilePaths.push(sourcePath);
+      }
     }
   });
   
-  var packageInfo = (helper.find(data, {kind: 'package'}) || [])[0];
-  console.log(packageInfo);
+  packageInfo = ( find({kind: 'package'}) || [] )[0];
+  if (packageInfo && packageInfo.name) {
+    outdir = path.join( outdir, packageInfo.name, (packageInfo.version || '') );
+  }
+  fs.mkPath(outdir);
   
-  console.log('MKDIR: ' + outdir);
-  mkdirpSync(outdir);
+  // copy the template's static files to outdir
+  /*
+  fromDir = path.join(templatePath, 'static');
+  staticFiles = fs.ls(fromDir, 3);
+
+  staticFiles.forEach(function(fileName) {
+    var toDir = fs.toDir( fileName.replace(fromDir, outdir) );
+
+    fs.mkPath(toDir);
+    fs.copyFileSync(fileName, toDir);
+  });
+  */
+  
+  if (sourceFilePaths.length) {
+    sourceFiles = shortenPaths( sourceFiles, path.commonPrefix(sourceFilePaths) );
+  }
+  data().each(function(doclet) {
+    var docletPath;
+    var url = helper.createLink(doclet);
+
+    helper.registerLink(doclet.longname, url);
+
+    // add a shortened version of the full path
+    if (doclet.meta) {
+      docletPath = getPathFromDoclet(doclet);
+      docletPath = sourceFiles[docletPath].shortened;
+      if (docletPath) {
+        doclet.meta.shortpath = docletPath;
+      }
+    }
+  });
+  
+  data().each(function(doclet) {
+    var url = helper.longnameToUrl[doclet.longname];
+
+    if (url.indexOf('#') > -1) {
+      doclet.id = helper.longnameToUrl[doclet.longname].split(/#/).pop();
+    }
+    else {
+      doclet.id = doclet.name;
+    }
+
+    if ( needsSignature(doclet) ) {
+      addSignatureParams(doclet);
+      addSignatureReturns(doclet);
+      addAttribs(doclet);
+    }
+  });
+  
   
   var members = helper.getMembers(data);
   //console.log(members);
   
   //console.log(taffy);
   
-  
-  data().each((doclet) => {
-    const url = helper.createLink(doclet, opts);
-    console.log('*: ' + url);
-    
-    helper.registerLink(doclet.longname, url);
-    
-  });
   
   
   data().each((doclet) => {
